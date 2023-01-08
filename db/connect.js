ac4af73d56db7e8853488d0d4c80507c201debfc
Rohith Challa <rohithsince2002@gmail.com>
2023-01-08 16:53:12 +0530

    update

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 require("dotenv").config()
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+  })
 .then(()=>{
     console.log("Connection Successful ...");
 })
